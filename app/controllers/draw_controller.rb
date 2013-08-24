00205f725ee1d2fe747d7f2b48b4580e83a34760
LyleE <lyleentwistle@gmail.com>
2013-08-24 19:12:48 +0100

    Upload to S3!

--- app/controllers/draw_controller.rb
+++ app/controllers/draw_controller.rb
@@ -5,10 +5,26 @@ class DrawController < ApplicationController
 	end
 
 	def save
-		File.open("testing.png","wb") do |f|
-			f.write(Base64.decode64(params[:data]));
-		end
+		image = Base64.decode64(params[:data]);
+
+		#File.open("testing.png","wb") do |f|
+		#	f.write(image);
+		#end
+
+		bucket.objects[getKey params[:target]].write(image)
+
 		redirect_to :root
+	end
+
+	private
+
+	def bucket
+		s3 = AWS::S3.new
+		s3.buckets['scribblescramble']
+	end
+
+	def getKey target
+		target.tr(' ','_')+"_"+(1000.0*Time.now.to_f).to_i.to_s+".png"
 	end
 
 end
