3d24624b8b0bf762c5f8da489e266650ea4b012f
LyleE <lyleentwistle@gmail.com>
2013-08-25 02:12:21 +0100

    The untested controller code for ELO rating changes

--- app/controllers/rate_controller.rb
+++ app/controllers/rate_controller.rb
@@ -3,14 +3,36 @@ class RateController < ApplicationController
 	def index
 		@type = ScribbleType.random_rateable
 		if(@type)
-			@scribbles = Scribble.where(scribble_type_id: @type.id).where("last_rated < ?", Time.now.to_i - 60).limit(2)
+			now = Time.now.to_i
+			@scribbles = Scribble.where(scribble_type_id: @type.id).where("last_rated < ?", now - 60).limit(2)
 
 			if(@scribbles.count == 2)
+				@scribbles.each do |s|
+					s.update(last_rated: now)
+				end
 				return
 			end
 		end
 		
 		redirect_to :play, :notice => 'No ratings available'
+	end
+
+	def save
+		winningScribble = Scribble.where(s3_key: params[:winner]).first
+		losingScribble = Scribble.where(s3_key: params[:loser]).first
+		if winningScribble and losingScribble
+			winner = winningScribble.user
+			loser = losingScribble.user
+
+			winnerOldRating = winner.rating
+			loserOldRating = loser.rating
+
+			winner.won_vs(loserOldRating)
+			loser.lost_vs(winnerOldRating)
+
+			winningScribble.destroy
+			losingScribble.destroy
+		end
 	end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,24 @@ class User < ActiveRecord::Base
 	validates_length_of :username, :minimum => 3, :message => "must be at least 3 characters long"
 	validates_length_of :password, :minimum => 6, :message => "must be at least 6 characters long", :if => :password
 	validates_confirmation_of :password, :message => "doesn't match", :if => :password
+
+	def won_vs other
+		update_rating 1.0 other
+	end
+
+	def lost_vs otherRating
+		update_rating 0.0 other
+	end
+
+	private
+
+	# http://en.wikipedia.org/wiki/Elo_rating_system#Mathematical_details
+	def update_rating actualResult otherRating
+		expectedResult = Q(rating) / ( Q(rating) + Q(otherRating) )
+		update(rating: rating + 32(actualResult - expectedResult))
+	end
+
+	def Q rating
+		10.0 ** (rating.to_f / 400.0)
+	end
 end
